0fa0e8b98d4e920207b861bb4c069933f329c8ee
BekzodGayratov <bekzodflutter@gmail.com>
2022-11-02 12:02:17 +0500

    Final code chagnes

--- lib/helpers/on_recieve_progress.dart
+++
@@ -1,7 +0,0 @@
-import 'package:request_handler/request_handler.dart';
-
-void main() async {
-  await DioRequestHandler.get(
-    url: "https://jsonplaceholder.typicode.com/userss",
-  ).then((value) {});
-}
